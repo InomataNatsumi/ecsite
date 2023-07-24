3019f67e0c712b418536f39f14318b62e063720f
InomataNatsumi <natsuminnie.913@live.jp>
2023-07-24 12:19:46 +0900

    Lesson_ecsite

---
+++ src/main/java/jp/co/internous/ecsite/model/form/HistoryForm.java
@@ -0,0 +1,15 @@
+package jp.co.internous.ecsite.model.form;
+
+import java.io.Serializable;
+
+public class HistoryForm implements Serializable{
+
+	private int userid;
+
+	public int getUserId() {
+		return userid;
+	}
+	public void setUserId(int userid) {
+		this.userid = userid;
+	}
+}
